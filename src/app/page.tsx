6d34dff6b2f5cab4a9c50a6a8210bc9bc35f6c64
kev-techi <kevin@techi.be>
2024-06-17 11:27:28 +0200

    make route dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Wallet from "@/containers/wallet";
 import { getConfig } from "@/services/config";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   const config = getConfig();
 

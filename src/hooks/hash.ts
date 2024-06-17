9978dba7124370bda9e356aee40cd42db8d8d918
kev-techi <kevin@techi.be>
2024-06-17 11:27:05 +0200

    safely access window

--- src/hooks/hash.ts
+++ src/hooks/hash.ts
@@ -1,12 +1,15 @@
 import { useSafeEffect } from "@citizenwallet/sdk";
 import { useState } from "react";
 
+export const getHash = () =>
+  typeof window !== "undefined" ? window.location.hash : "";
+
 export const useHash = () => {
-  const [hash, setHash] = useState(window.location.hash);
+  const [hash, setHash] = useState(getHash());
 
   useSafeEffect(() => {
     const onHashChange = () => {
-      setHash(window.location.hash);
+      setHash(getHash());
     };
 
     window.addEventListener("hashchange", onHashChange);

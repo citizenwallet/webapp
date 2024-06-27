04831740c13bbe9287ed0fa48bca4f645829e4fd
kev-techi <kevin@techi.be>
2024-06-27 11:38:23 +0200

    dynamic path

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -2,7 +2,8 @@ import { Config } from "@citizenwallet/sdk";
 import { existsSync, readFileSync } from "fs";
 import path from "path";
 
-const communityPath = process.env.COMMUNITY_JSON_PATH || "community.json";
+const getCommunityPath = () =>
+  process.env.COMMUNITY_JSON_PATH || "community.json";
 
 export const readCommunityFile = (): Config | undefined => {
   if (!communityFileExists()) {
@@ -10,13 +11,13 @@ export const readCommunityFile = (): Config | undefined => {
   }
 
   // read community.json file
-  const filePath = path.join(process.cwd(), communityPath);
+  const filePath = path.join(process.cwd(), getCommunityPath());
   const fileContents = readFileSync(filePath, "utf8");
   const config = JSON.parse(fileContents) as Config;
   return config;
 };
 
 export const communityFileExists = (): boolean => {
-  const filePath = path.join(process.cwd(), communityPath);
+  const filePath = path.join(process.cwd(), getCommunityPath());
   return existsSync(filePath);
 };

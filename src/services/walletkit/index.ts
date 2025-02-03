cbcfce6ed83e8367fbcf0447bab50d103ce40749
sajee_techi <sajeewa@techi.be>
2025-02-03 18:45:34 +0530

    wallet kit supported methods and events

--- src/services/walletkit/index.ts
+++ src/services/walletkit/index.ts
@@ -12,8 +12,36 @@ import {
   AuthTypes,
 } from "@walletconnect/types";
 
-export const SUPPORTED_METHODS = ["eth_sendTransaction", "personal_sign"];
-export const SUPPORTED_EVENTS = ["accountsChanged", "chainChanged"];
+export const SUPPORTED_METHODS = [
+  "eth_accounts",
+  "eth_requestAccounts",
+  "eth_sendRawTransaction",
+  "eth_sign",
+  "eth_signTransaction",
+  "eth_signTypedData",
+  "eth_signTypedData_v3",
+  "eth_signTypedData_v4",
+  "eth_sendTransaction",
+  "personal_sign",
+  "wallet_switchEthereumChain",
+  "wallet_addEthereumChain",
+  "wallet_getPermissions",
+  "wallet_requestPermissions",
+  "wallet_registerOnboarding",
+  "wallet_watchAsset",
+  "wallet_scanQRCode",
+  "wallet_sendCalls",
+  "wallet_getCallsStatus",
+  "wallet_showCallsStatus",
+  "wallet_getCapabilities",
+];
+export const SUPPORTED_EVENTS = [
+  "chainChanged",
+  "accountsChanged",
+  "message",
+  "disconnect",
+  "connect",
+];
 
 class WalletKitService {
   private static instance: WalletKitService | null = null;

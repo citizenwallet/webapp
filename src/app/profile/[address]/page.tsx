d958259d48f628826e4179a41021120d705cb0a3
kev-techi <kevin@techi.be>
2025-03-13 20:44:11 +0100

    metadata based on community

--- src/app/profile/[address]/page.tsx
+++ src/app/profile/[address]/page.tsx
@@ -11,8 +11,42 @@ import {
 import { Suspense } from "react";
 import { ZeroAddress } from "ethers";
 import { headers } from "next/headers";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const metadata: Metadata = {
+    title: "Citizen Wallet",
+    description: "The open source wallet for your community.",
+    icons: {
+      icon: "/favicon.ico",
+    },
+    openGraph: {
+      title: "Citizen Wallet",
+      description: "The open source wallet for your community.",
+      images: ["/logo.png"],
+    },
+  };
+
+  const headersList = await headers();
+
+  const config = await getCommunityFromHeaders(headersList);
+  if (!config) {
+    return metadata;
+  }
+
+  metadata.title = config.community.name;
+  metadata.description = config.community.description;
+  metadata.openGraph = {
+    title: config.community.name,
+    description: config.community.description,
+    images: [config.community.logo],
+    type: "article",
+  };
+
+  return metadata;
+}
 
 interface PageProps {
   params: Promise<{

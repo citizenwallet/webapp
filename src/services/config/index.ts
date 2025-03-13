bbd592255a909dba6a9874c13355686a8455a7d7
kev-techi <kevin@techi.be>
2025-03-13 19:58:29 +0100

    fix communities for prod

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,38 +1,35 @@
 import { Config, parseAliasFromDomain } from "@citizenwallet/sdk";
-import { existsSync, readFileSync } from "fs";
 import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
-import path from "path";
 
-const getCommunityPath = () =>
-  process.env.COMMUNITIES_JSON_PATH || "communities.json";
+import CommunitiesJson from "./communities.json";
+import LocalCommunitiesJson from "./communities.local.json";
 
-export const readCommunityFile = (
+const getCommunityFile = async (): Promise<Config[]> => {
+  if (process.env.NODE_ENV === "production") {
+    return CommunitiesJson as unknown as Config[];
+  }
+
+  return LocalCommunitiesJson as unknown as Config[];
+};
+
+export const readCommunityFile = async (
   _alias = process.env.FALLBACK_COMMUNITY_ALIAS ?? ""
-): Config | undefined => {
+): Promise<Config | undefined> => {
   let alias = _alias;
   if (process.env.NODE_ENV === "development") {
-    // when developing locally, we use the fallback alias since localhost won't resolve to a community
     alias = process.env.FALLBACK_COMMUNITY_ALIAS ?? "";
   }
 
-  if (!communityFileExists()) {
+  const configs = await getCommunityFile();
+  if (!configs) {
     return undefined;
   }
 
-  // read community.json file
-  const filePath = path.join(process.cwd(), getCommunityPath());
-  const fileContents = readFileSync(filePath, "utf8");
-  const configs = JSON.parse(fileContents) as Config[];
   if (configs.length === 0) {
     return undefined;
   }
 
   return configs.find((c) => c.community.alias === alias);
-};
-
-export const communityFileExists = (): boolean => {
-  const filePath = path.join(process.cwd(), getCommunityPath());
-  return existsSync(filePath);
 };
 
 export const getCommunityFromHeaders = async (

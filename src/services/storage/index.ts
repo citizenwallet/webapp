02134f984c1881aa060bce68c6ae3f3fee4cb31b
sajee_techi <sajeewa@techi.be>
2025-04-17 18:16:00 +0530

    local storage keys

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -4,11 +4,24 @@ export class StorageService {
     this.alias = alias;
   }
 
-  setKey(key: string, value: string) {
+  setKey(key: StorageKey, value: string) {
     localStorage.setItem(`${this.alias}_${key}`, value);
   }
 
-  getKey(key: string) {
+  getKey(key: StorageKey) {
     return localStorage.getItem(`${this.alias}_${key}`);
   }
 }
+
+
+export const StorageKeys = {
+  SESSION_PRIVATE_KEY: "session_private_key",
+  SESSION_HASH: "session_hash",
+  SESSION_SOURCE_VALUE: "session_source_value",
+  SESSION_SOURCE_TYPE: "session_source_type",
+
+
+  HASH: "hash", //hash of local accounts
+} as const;
+
+export type StorageKey = keyof typeof StorageKeys;
